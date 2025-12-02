f251ff21925573035333f48363d376f75fda504f
balatrower <power333xd@gmail.com>
2025-12-02 10:27:30 +0100

    initTypes updated

--- initTypes.cpp
+++ initTypes.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
 int main() {
-    int numero = 1.5; //copy initialization, narrowing conversion habilitado
+    int numero1 = 1.5; //copy initialization, narrowing conversion habilitado pero al tratar warnings como errores el compilador da error
 
-    int numero2 ( 2.5 ); //direct initialization, narrowing conversion habilitado
+    int numero2 ( 2.2 ); //direct initialization, narrowing conversion habilitado pero al tratar warnings como errores el compilador da error
 
-    int numero3 = { 3.5 }; //copy list initialization (raramente usado), narrowing conversion no habilitado 
+    int numero3 = { 3 }; //copy list initialization (raramente usado), narrowing conversion no habilitado 
 
-    int numero4 { 4.5 }; //direct list initialization (el que se usara), narrowing conversion no habilitado
+    int numero4 { 4 }; //direct list initialization (el que se usara), narrowing conversion no habilitado
+
+    std::cout << numero1 << "\n" << numero2 << "\n" << numero3 << "\n"  << numero4;
+
+    return 0;
 }
